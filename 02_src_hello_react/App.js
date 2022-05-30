3a2cc1a9906b26d7f15383c6de9c3e14d3c1a6e5
phoebe0412 <phoebe19810412@gmail.com>
2022-05-30 16:27:41 +0800

    todolist 靜態組件

---
+++ 02_src_hello_react/App.js
@@ -0,0 +1,16 @@
+// 創建外殼組件
+import React,{Component} from 'react'
+import Hello from './components/Hello'
+import Welcome from './components/Welcome'
+
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+        <Hello/>
+        <Welcome/>
+      </div>
+    )
+  }
+}
+
