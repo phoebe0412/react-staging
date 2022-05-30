5ab3ae9ef11cef2a99b280f407eba6699bbf662b
phoebe0412 <phoebe19810412@gmail.com>
2022-05-30 17:00:28 +0800

    todolist 狀態初始化列表

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import Footer from "./components/Footer";
 import './App.css'
 
 export default class App extends Component {
+
+  state = {
+    todos:[
+      {id:'001',name:'吃飯',done:true},
+      {id:'002',name:'睡覺',done:false},
+      {id:'003',name:'聽音樂',done:true},
+      {id:'004',name:'去海邊',done:false}
+    ],
+  }
+
   render() {
+    const {todos} = this.state
     return (
       <div className="todo-container">
         <div className="todo-wrap">
           <Header/>
-          <List/>
+          <List todos={todos}/>
           <Footer/>
         </div>
       </div>
