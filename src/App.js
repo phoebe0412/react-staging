4655c66c7fb6d52611d1dd99bac022363e6a37f8
phoebe0412 <phoebe19810412@gmail.com>
2022-05-31 15:56:09 +0800

    todolist 添加一個todo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Footer from "./components/Footer";
 import './App.css'
 
 export default class App extends Component {
-
+  // 狀態在哪裡,操作狀態的方法就在哪裡
+  
   state = {
     todos:[
       {id:'001',name:'吃飯',done:true},
@@ -24,6 +25,18 @@ export default class App extends Component {
     // 更新狀態
     this.setState({todos:newTodos})
   }
+  
+  // 用於更新一個todo對象
+  updateTodo = (id, done) =>{
+    // 獲取狀態中的todos
+    const {todos} = this.state
+    // 匹配處理數據
+    const newTodos = todos.map((todoObj)=>{
+      if(todoObj.id === id) return {...todoObj, done}
+      else return todoObj
+    })
+    this.setState({todos:newTodos})
+  }
 
   render() {
     const {todos} = this.state
@@ -31,7 +44,7 @@ export default class App extends Component {
       <div className="todo-container">
         <div className="todo-wrap">
           <Header addTodo={this.addTodo}/>
-          <List todos={todos}/>
+          <List todos={todos} updateTodo={this.updateTodo}/>
           <Footer/>
         </div>
       </div>
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -5,19 +5,27 @@ export default class Item extends Component {
 
   state = {mouse:false}
 
+  // 滑鼠移入與移出
   handleMouse = (flag) =>{
     return ()=>{
       this.setState({mouse:flag})
     }
   }
+
+  // 勾選與取消勾選
+  handleCheck = (id) =>{
+    return (event)=>{
+      this.props.updateTodo(id, event.target.checked)
+    }
+  }
   
   render() {
-    const {name,done} = this.props
+    const {id,name,done} = this.props
     const {mouse} = this.state
     return (
       <li style={{backgroundColor:mouse ? '#ddd':'#FFF'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)} >
         <label>
-          <input type="checkbox" defaultChecked={done}/>
+          <input type="checkbox" defaultChecked={done} onChange={this.handleCheck(id)}/>
           <span>{name}</span>
         </label>
         <button className="btn btn-danger" style={{display:mouse? 'block':'none'}}>
