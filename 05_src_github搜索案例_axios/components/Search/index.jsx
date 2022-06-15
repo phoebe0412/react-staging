cd4b9d980dc5d10f5d87ad4a389f0ba8795541a3
phoebe0412 <phoebe19810412@gmail.com>
2022-06-15 16:18:02 +0800

    github搜索案例(axios)

---
+++ 05_src_github搜索案例_axios/components/Search/index.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class Search extends Component {
+
+  search = () => {
+    // 獲取用戶的輸入(連續解構賦值+重新命名)
+    const {keyWordElement:{value:keyWord}} = this
+    // 發送請求前通知App更新狀態
+    this.props.updateAppState({isFirst:false,isLoading:true})
+    // 發送網路請求
+    axios.get(`/api1/search/users?q=${keyWord}`).then(
+      response => {
+        // 請求成功後通知App更新狀態
+        this.props.updateAppState({isLoading:false,users:response.data.items})
+      },
+      error => {
+        // 請求失敗後通知App更新狀態
+        this.props.updateAppState({isLoading:false,err: error.message})
+      }
+    )
+  }
+  render() {
+    return (
+      <section className="jumbotron">
+        <h3 className="jumbotron-heading">搜尋github用戶</h3>
+        <div>
+          <input ref={c => this.keyWordElement = c} type="text" placeholder="輸入關鍵詞點擊搜尋"/>&nbsp;
+          <button onClick={this.search}>搜尋</button>
+        </div>
+      </section>
+    )
+  }
+}
