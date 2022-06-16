ebf877d55a11e114119ff751a52c1e6a7aa2a853
phoebe0412 <phoebe19810412@gmail.com>
2022-06-16 14:30:25 +0800

    github搜索案例 (pubsub)

---
+++ 06_src_github搜索案例_pubsub/index.js
@@ -0,0 +1,14 @@
+// 引入react核心庫
+import React from 'react';
+// 引入ReactDOM
+import ReactDOM from 'react-dom/client';
+// 引入App組件
+import App from './App';
+
+// 渲染App到頁面
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
