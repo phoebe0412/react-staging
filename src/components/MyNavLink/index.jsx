c6a01b91c8d548e2bee2d21f40ab949fdf2b4a57
phoebe.chou <phoebe.chou@goldnetit.com>
2022-07-12 16:09:33 +0800

    封裝NavLink

---
+++ src/components/MyNavLink/index.jsx
@@ -0,0 +1,11 @@
+import React, { Children, Component } from 'react'
+import { NavLink } from "react-router-dom";
+
+export default class MyNavLink extends Component {
+  render() {
+		console.log(this.props)
+    return (
+      <NavLink activeClassName="activeColor" className="list-group-item" {...this.props} />
+    )
+  }
+}
